743facf3061717ef9daf70fd815f03cf59a7e3b8
Stefan <stefangrebenar@hotmail.com>
2018-04-19 13:13:07 +0200

    Hoi

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+
 	<title>Brochure aanvraag</title>
 	<link rel="stylesheet" type="text/css" href="foundation.css">
 	<link rel="stylesheet" type="text/css" href="app.css">
+
 </head>
+
 <body>
 
 
 
-	<form method="get" action="HTML en PHP.php">
-				E-mailadres:	<input type="text" name="E-mail"><br>
-                Onderwerp:		<input type="text" name="Onderwerp"><br>
-                Bericht: 		<textarea type="text" name="Bericht"></textarea><br>
-		<input type="submit" value="Verzenden" name="knop">
-	</form>
-
-	<br>
-
-
 </body>
+
 </html>
 
 
