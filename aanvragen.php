666d2344456f0015df2f72e74f3961f0add4c214
Stefan Grebenar <stefangrebenar@hotmail.com>
2018-04-20 12:23:39 +0200

    20/04/2018 Update

--- aanvragen.php
+++ aanvragen.php
@@ -3,28 +3,73 @@
 
 <head>
 
-	<title>Brochure aanvraag</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+    <title>Brochure aanvraag</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+    <?php include ("header.php");
+
+    ?>
+
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<form>
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <h4>Persoonlijke gegevens</h4>
+        <label for="Voornaam">Voornaam*</label>
+        <input type="text" class="form-control" id="Voornaam" placeholder="Vul voornaam in...">
+    </div>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="http://www.windesheim.nl">Home<span class="sr-only"></span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="aanvragen.php">Brochure aanvragen</a>
-        </ul>
-</nav>
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <label for="Tussenvoegsels">Tussenvoegsels</label>
+        <input type="text" class="form-control" id="Tussenvoegsesls" placeholder="(optioneel)">
+    </div>
+
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <label for="Achternaam">Achternaam*</label>
+        <input type="text" class="form-control" id="Achternaam" placeholder="Vul achternaam in...">
+    </div>
+
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <label for="eMailadres">E-Mailadres*</label>
+        <input type="email" class="form-control" id="eMailadres" placeholder="Vul e-mailadres in...">
+    </div>
+
+</form>
+
+<form>
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <h4>Vooropleiding</h4>
+        <label for="Opleiding">Opleiding*</label>
+        <input type="text" class="form-control" id="Opleiding" placeholder="Vul je opleiding in...">
+    </div>
+
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <label for="verwachtAfstudeerJaar">(verwacht) Afstudeerjaar*</label>
+        <select class="form-control" id="verwachtAfstudeerJaar">
+            <option>2020</option>
+            <option>2019</option>
+            <option>2018</option>
+            <option>Al afgestudeerd</option></select>
+    </div>
+</form>
+
+<form>
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <h4>Kies de gewenste opleiding</h4>
+        <p><b>ICT</b></p>
+        <input type="radio" class="form-check-input" id="hboIct">
+        <label class="form-check-label" for="hboIct">De enige opleiding die er toe doet, HBO-ICT</label>
+    </div>
+</form>
+
+<form>
+    <div class="container col-md-4 col-md-offset-4 my-4">
+        <button type="submit" class="btn btn-dark">Verzend</button>
+
+</form>
 
 </body>
 
---
+++ header.php
@@ -0,0 +1,40 @@
+<style>
+
+    body, html {
+        height: 60%;
+
+    }
+
+    .bg {
+
+        background-image: url("Gebouw-x-windesheim.jpg");
+
+        height: 100%;
+
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+</style>
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+    <a class="navbar-brand" href="logo">
+        <img src="logo.png" alt="Logo" style="width:40px">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="http://www.ishetaltijdvoorbier.nl/">Home<span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="aanvragen.php">Brochure aanvragen</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="bg"></div>
